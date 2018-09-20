db13b6c8cffa503cc8f94289e3ad947787863826
Emanuel Bierman <emanuel.bierman@gmail.com>
2018-09-20 23:57:17 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -14,6 +14,7 @@ class FiguresController < ApplicationController
   end
 
   post '/figures' do
+    binding.pry
     @figure = Figure.create(params[:figure])
     redirect "figures/#{@figure.id}"
   end
